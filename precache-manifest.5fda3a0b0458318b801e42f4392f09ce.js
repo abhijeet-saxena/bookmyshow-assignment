d777af73e9ecc0fb60c8fcc2b8948855ed88a53f
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2019-06-21 00:59:13 +0530

    Updates

---
+++ precache-manifest.5fda3a0b0458318b801e42f4392f09ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df55bdb800017f02dc4fc76d4f9496c",
+    "url": "/bookmyshow-assignment/index.html"
+  },
+  {
+    "revision": "108368fc4e7034405c7a",
+    "url": "/bookmyshow-assignment/static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/bookmyshow-assignment/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "108368fc4e7034405c7a",
+    "url": "/bookmyshow-assignment/static/js/main.531c5d59.chunk.js"
+  },
+  {
+    "revision": "7f8f253b24c88ccfb9a2",
+    "url": "/bookmyshow-assignment/static/js/runtime~main.d9165e75.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bookmyshow-assignment/static/media/logo.5d5d9eef.svg"
+  }
+]);
